4ea9fd492f387a6d04525d1f08c4851898457140
Francois Kosie <francois.kosie@gmail.com>
2025-02-09 17:29:46 -0500

    adding shop, cart context, shopping icon, as well as cart dropdown

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState } from 'react';
+
+import PRODUCTS from '../shop-data.json';
+
+export const ProductsContext = createContext({products: PRODUCTS});
+
+export const ProductsProvider = ({children}) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  return <ProductsContext.Provider value={{products}}>{children}</ProductsContext.Provider>
+}
